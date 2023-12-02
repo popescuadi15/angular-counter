4e0b3688e3c86189c167639de420c5ce35b3be00
popescuadi15 <florian.popescu@student.upt.ro>
2023-12-02 22:47:16 +0200

    Initial commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  count = 0;
+
+  increment(){
+    this.count++;
+  }
+
+  decrement(){
+    this.count--;
+  }
+}
